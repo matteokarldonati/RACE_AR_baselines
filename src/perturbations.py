df59e0c0c691dd161dd8743f5f419d2943fb7c12
Matteo Karl Donati <donati.matteo.k@gmail.com>
2020-08-21 15:54:42 +0200

    perturbations test

---
+++ src/perturbations.py
@@ -0,0 +1,111 @@
+import os
+import pickle
+import random
+import sys
+
+import neuralcoref
+import nltk
+import spacy
+from nltk.corpus import names
+
+nlp = spacy.load("en_core_web_sm")
+neuralcoref.add_to_pipe(nlp)
+
+nltk.download('names')
+
+MALE_NAMES = names.words('male.txt')
+FEMALE_NAMES = names.words('female.txt')
+
+NAMES = MALE_NAMES + FEMALE_NAMES
+
+path = [path for path in sys.path if path.endswith('RACE_AR_baselines/src')][0]
+ner_dict_path = os.path.join(path, 'data_perturbations/ner_dict.pkl')
+with open(ner_dict_path, 'rb') as f:
+    NER_DICT = pickle.load(f)
+
+
+def get_names(text):
+    doc = nlp(text)
+    names = []
+    for x in doc.ents:
+        if x.label_ == 'PERSON':
+            names.append(x.text)
+
+    names = list(set(names))
+    return names
+
+
+def get_names_groups(text):
+    doc = nlp(text)
+    clusters = doc._.coref_clusters
+    groups = []
+    if clusters:
+        for cluster in clusters:
+            name_group = get_names('. '.join(list(set([str(i) for i in cluster.mentions]))))
+
+            flag = True
+            for name in name_group:
+                for group in groups:
+                    if name in group:
+                        flag = False
+                    elif name in ' '.join(group):
+                        group.append(name)
+                        flag = False
+
+            if name_group and flag:
+                groups.append(name_group)
+
+    return groups
+
+
+def get_entities(text, entity_type):
+    doc = nlp(text)
+    entities = []
+    for x in doc.ents:
+        if x.label_ == entity_type:
+            entities.append(x.text)
+
+    entities = list(set(entities))
+    return entities
+
+
+def get_adv_names(names_num, name_gender_or_race):
+    adv_names = []
+    for _ in range(names_num):
+        if name_gender_or_race == 'males':
+            adv_names.append(random.choice(MALE_NAMES))
+        elif name_gender_or_race == 'female':
+            adv_names.append(random.choice(FEMALE_NAMES))
+        else:
+            adv_names.append(random.choice(NAMES))
+    return adv_names
+
+
+def get_adv_entities(entities_num, entity_type):
+    adv_entities = []
+    for _ in range(entities_num):
+        adv_entities.append(random.choice(NER_DICT[entity_type]))
+    return adv_entities
+
+
+def replace_names(text, names, adv_names):
+    if not names:
+        return text
+
+    for i, name_group in enumerate(names):
+        for name in name_group:
+            adv_text = text.replace(name, adv_names[i])
+            text = adv_text
+
+    return adv_text
+
+
+def replace_entities(text, entities, adv_entities):
+    if not entities:
+        return text
+
+    for i, entity in enumerate(entities):
+        adv_text = text.replace(entity, adv_entities[i])
+        text = adv_text
+
+    return adv_text
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,25 +1,36 @@
-import os
 import json
+import nltk
+import os
 from nltk import sent_tokenize, word_tokenize
+
+from perturbations import get_names_groups, get_adv_names, replace_names, get_entities, get_adv_entities, \
+    replace_entities
+
+nltk.download('punkt')
 
 
 def tokenize(st):
-    #TODO: The tokenizer's performance is suboptimal
+    # TODO: The tokenizer's performance is suboptimal
     ans = []
     for sent in sent_tokenize(st):
         ans += word_tokenize(sent)
     return " ".join(ans).lower()
 
+
 if __name__ == "__main__":
     difficulty_set = ["middle", "high"]
     data = "../data/data"
-    raw_data = "../data/RACE"
+    raw_data = "../../drive/My Drive/Colab Notebooks/noahlab/RACE"
     cnt = 0
     avg_article_length = 0
     avg_question_length = 0
     avg_option_length = 0
     num_que = 0
-    for data_set in ["train", "dev", "test"]:
+
+    perturbation_num = 25
+    perturbation_type = 'names'
+
+    for data_set in ["test"]:
         p1 = os.path.join(data, data_set)
         if not os.path.exists(p1):
             os.mkdir(p1)
@@ -30,18 +41,51 @@ if __name__ == "__main__":
             new_raw_data_path = os.path.join(raw_data, data_set, d)
             for inf in os.listdir(new_raw_data_path):
                 cnt += 1
+
                 obj = json.load(open(os.path.join(new_raw_data_path, inf), "r"))
-                obj["article"] = obj["article"].replace("\\newline", "\n")
-                obj["article"] = tokenize(obj["article"])
-                avg_article_length += obj["article"].count(" ")
-                for i in range(len(obj["questions"])):
-                    num_que += 1
-                    obj["questions"][i] = tokenize(obj["questions"][i])
-                    avg_question_length += obj["questions"][i].count(" ")
-                    for k in range(4):
-                        obj["options"][i][k] = tokenize(obj["options"][i][k])
-                        avg_option_length += obj["options"][i][k].count(" ")
-                json.dump(obj, open(os.path.join(new_data_path, inf), "w"), indent=4)
+
+                article = obj["article"]
+
+                if perturbation_type == 'names':
+                    names = get_names_groups(article)
+
+                if perturbation_type in ['ORG', 'GPE', 'LOC', 'NORP']:
+                    entities = get_entities(article, perturbation_type)
+
+                for _ in range(perturbation_num):
+
+                    if perturbation_type == 'names':
+                        adv_names = get_adv_names(len(names), None)
+                        obj["article"] = replace_names(article, names, adv_names)
+                    if perturbation_type in ['ORG', 'GPE', 'LOC', 'NORP']:
+                        adv_entities = get_adv_entities(len(entities), perturbation_type)
+                        obj["article"] = replace_entities(article, entities, adv_entities)
+
+                    obj["article"] = obj["article"].replace("\\newline", "\n")
+                    obj["article"] = tokenize(obj["article"])
+                    avg_article_length += obj["article"].count(" ")
+                    for i in range(len(obj["questions"])):
+                        num_que += 1
+
+                        if perturbation_type == 'names':
+                            obj["questions"][i] = replace_names(obj["questions"][i], names, adv_names)
+
+                        if perturbation_type in ['ORG', 'GPE', 'LOC', 'NORP']:
+                            obj["questions"][i] = replace_entities(data_raw["questions"][i], entities, adv_entities)
+
+                        obj["questions"][i] = tokenize(obj["questions"][i])
+                        avg_question_length += obj["questions"][i].count(" ")
+                        for k in range(4):
+
+                            if perturbation_type == 'names':
+                                obj["options"][i][k] = replace_names(obj["options"][i][k], names, adv_names)
+
+                            if perturbation_type in ['ORG', 'GPE', 'LOC', 'NORP']:
+                                obj["options"][i][k] = replace_entities(obj["options"][i][k], entities, adv_entities)
+
+                            obj["options"][i][k] = tokenize(obj["options"][i][k])
+                            avg_option_length += obj["options"][i][k].count(" ")
+                    json.dump(obj, open(os.path.join(new_data_path, inf), "w"), indent=4)
     '''print "avg article length", avg_article_length * 1. / cnt
     print "avg question length", avg_question_length * 1. / num_que
     print "avg option length", avg_option_length * 1. / (num_que * 4)'''
